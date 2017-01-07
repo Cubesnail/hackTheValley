e018d15f68be20e8159a7a1b5ea20871c40e6005
zakgy <ztcgy11@gmail.com>
2017-01-07 15:40:52 -0500

    adding text to dctionary converter

---
+++ dictionaryConverter.py
@@ -0,0 +1,12 @@
+swipeDict = {'hgfrerjklo': 'hello'}
+
+f = open ('filename**', 'r')
+
+for line in f:
+    firstspace = line.find(' ', beg=0, end=len(line))
+
+    value = line[0:firstspace]
+    key = line[firstspace + 1:len(line)]
+
+    if not swipeDict.has_key(key):
+        swipeDict[key] = value;
