a17dc6aef120460213bd7fea002ce0fb0fceb40f
zakgy <ztcgy11@gmail.com>
2017-01-07 19:45:38 -0500

    100 words now

--- dictionaryConverter.py
+++ dictionaryConverter.py
@@ -5,8 +5,8 @@ f = open ('dictionary.txt', 'r')
 for line in f:
     firstspace = line.find(' ', beg=0, end=len(line))
 
-    value = line[0:firstspace]
-    key = line[firstspace + 1:len(line)]
+    value = line[0:firstspace].lower
+    key = line[firstspace + 1:len(line)].lower
 
     if not swipeDict.has_key(key):
         swipeDict[key] = value;
